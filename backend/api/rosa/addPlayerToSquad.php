0d13bcbd0e688a11619c5eaaf9bd7fbfbcd19a3e
michele25-10 <michele.gabri01@gmail.com>
2023-02-28 14:08:51 +0100

    API addPlayerToSquad
    I giocatori vengono aggiunti ad una squadra, uno alla volta

---
+++ backend/api/rosa/addPlayerToSquad.php
@@ -0,0 +1,29 @@
+<?php
+require("../../common/connect.php");
+require("../../model/league.php");
+
+header("Content-type: application/json; charset=UTF-8");
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (empty($data->id_squad) || empty($data->id_league) || empty($data->id_player)) {
+    http_response_code(400);
+    echo json_encode(["message" => "Fill every field"]);
+    die();
+}
+
+$db = new Database();
+$db_conn = $db->connect();
+$rosa = new Rosa($db_comm);
+
+$result = $rosa->addPlayerToSquad($data->id_squad, $data->id_league, $data->id_player);
+
+if ($result != false) {
+    http_response_code(200);
+    echo json_encode(["response" => true]);
+} else {
+    http_response_code(401);
+    echo json_encode(["response" => false]);
+}
+die();
+?>
--- backend/model/rosa.php
+++ backend/model/rosa.php
@@ -1,6 +1,17 @@
 <?php
 class Rosa
 {
+    protected $conn;
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
+    function addPlayerToSquad($id_squad, $id_league, $id_player)
+    {
+        $sql = "INSERT INTO fantacalcio.rosa (id_squad, id_league, id_player)
+        VALUES ('" . $id_squad . "', '" . $id_league . "', '" . $id_player . "');";
+        return $sql;
+    }
 }
 ?>
