f59d5dc6e3bcba5a06a76a648ba5c55f7366e196
michele25-10 <michele.gabri01@gmail.com>
2023-02-28 14:26:22 +0100

    API getArchivePlayer
    Utile quando il trustee dovrà inserire o meno all'interno delle squadre i giocatori, sarà il risultato di un selected

---
+++ backend/api/player/getArchivePlayer.php
@@ -0,0 +1,39 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once dirname(__FILE__) . '/../../common/connect.php';
+include_once dirname(__FILE__) . '/../../model/player.php';
+include_once dirname(__FILE__) . '/../../model/base.php';
+
+$dtbase = new Database();
+$db_conn = $dtbase->connect();
+
+$player = new Player($db_conn);
+$query = $player->getArchivePlayer();
+$result = $conn->query($query);
+
+if (mysqli_num_rows($result) > 0) {
+    $players_arr = array();
+    while ($row = $result->fetch_assoc()) {
+        extract($row);
+        $player_arr = array(
+            'id' => $id,
+            'name' => $name,
+            'surname' => $id_trustee,
+            'role' => $role,
+        );
+        array_push($players_arr, $player_arr);
+    }
+    http_response_code(200);
+    echo (json_encode($players_arr, JSON_PRETTY_PRINT));
+} else {
+    http_response_code(400);
+    echo json_encode(["message" => "Non sono state trovate leghe"]);
+}
+
+
+$conn->close();
+die();
+?>
--- backend/model/player.php
+++ backend/model/player.php
@@ -1,6 +1,20 @@
 <?php
 class Player
 {
+    protected $conn;
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
+    function getArchivePlayer()
+    {
+        $sql = "SELECT id, name, surname, `role`
+              FROM player
+              WHERE 1=1
+              order by surname ASC;
+                ";
+        return $sql;
+    }
 }
 ?>
