3f50aa71b263df5c014af6402e3a376445e2611a
michele25-10 <michele.gabri01@gmail.com>
2023-03-10 14:48:31 +0100

    Continuata API createDayMatch()

--- backend/api/match/createDayMatch.php
+++ backend/api/match/createDayMatch.php
@@ -16,23 +16,61 @@ if (!isset($_GET['id_league']) || ($id_league = explode("?id_league=", $_SERVER[
 $dtbase = new Database();
 $conn = $dtbase->connect();
 
-$league = new Squad_League($conn);
-$query = $league->getSquadJoinLeagueMatch($id_league);
+$match = new Matches($conn);
+$query = $match->getLastNumberMatch($id_league);
 $result = $conn->query($query);
 
 if (mysqli_num_rows($result) > 0) {
-    $squads_arr = array();
     while ($row = $result->fetch_assoc()) {
         extract($row);
-        $squad_arr = $id;
-        array_push($squads_arr, $squad_arr);
+        $number_match = $numbermatch;
     }
+} else {
+    $number_match = 0;
 }
-$squads2_arr = $squads_arr;
 
-var_dump($squads_arr);
-shuffle($squads2_arr);
-var_dump($squads2_arr);
+if ($number_match == 38) {
+    //termina la lega
+    //updateStatus
+} else {
+    $league = new Squad_League($conn);
+    $query = $league->getSquadJoinLeagueMatch($id_league);
+    $result = $conn->query($query);
 
+    if (mysqli_num_rows($result) > 0) {
+        $squads_arr = array();
+        while ($row = $result->fetch_assoc()) {
+            extract($row);
+            $squad_arr = $id;
+            array_push($squads_arr, $squad_arr);
+        }
+    }
+    $squads2_arr = $squads_arr;
+    shuffle($squads2_arr);
+    $lunghezza = count($squads_arr);
+
+    for ($x = 0; $x < $lunghezza; $x) {
+        if ($squads_arr[$x] == $squads2_arr[$x]) {
+            $score1 = rand(0, 150);
+            $id_squad1 = $squads_arr[$x];
+            $number_match = $number_match + 1;
+
+            //uno scontro da solo
+        } else {
+            // query di inserimento nel db della giornata
+            $score1 = rand(0, 150);
+            $score2 = rand(0, 150);
+            $number_match = $number_match + 1;
+            $id_squad1 = $squads_arr[$x];
+            $id_squad2 = $squads2_arr[$x];
+
+            $query = $match->createMatch($number_match, $id_squad1, $id_squad2, $score1, $score2, $id_league);
+            var_dump($query);
+            $result = $conn->query($query);
+
+        }
+    }
+
+}
 
 ?>
--- backend/model/match.php
+++ backend/model/match.php
@@ -21,5 +21,16 @@ class Matches
         WHERE number_match = ( SELECT max(number_match) FROM match )";
         return $sql;
     }
+    function getLastNumberMatch($id_league)
+    {
+        $sql = "SELECT max(number_match) as 'numbermatch' FROM fantacalcio.`match` where id_league='" . $id_league . "';";
+        return $sql;
+    }
+    function createMatch($number_match, $id_squad1, $id_squad2, $score1, $score2, $id_league)
+    {
+        $sql = "insert into fantacalcio.match(number_match, id_squad1, id_squad2, score1, score2, league)
+                values('" . $number_match . "', '" . $id_squad1 . "', '" . $id_squad2 . "', '" . $score1 . "', '" . $score2 . "', '" . $id_league . "');";
+        return $sql;
+    }
 }
 ?>
